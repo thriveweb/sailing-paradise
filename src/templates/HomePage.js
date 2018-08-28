8171d10ffcc41751b8bbe3bf9a59e533bac18e72
Amber Gowans <amber@thriveweb.com.au>
2018-08-29 09:46:41 +1000

    update video

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -38,7 +38,84 @@ export const pageQuery = graphql`
       frontmatter {
         title
         featuredImage {
-          ...FluidImage
+          id
+        }
+        buttonTitle
+        buttonUrl
+        featuredSlider {
+          description
+          title
+          buttonUrl
+        }
+        featuredBanner {
+          title
+          buttonTitle
+          buttonUrl
+          image {
+            id
+          }
+        }
+        services {
+          serviceContent {
+            icon {
+              id
+            }
+            description
+            title
+            buttonUrl
+          }
+          image {
+            id
+          }
+        }
+        serviceBanner {
+          subtitle
+          title
+          buttonTitle
+          buttonUrl
+          featuredImage {
+            id
+          }
+        }
+        secondaryBanner {
+          buttonTitle
+          buttonUrl
+          title
+          subtitle
+          featuredImage {
+            id
+          }
+        }
+        aboutSection {
+          content
+          title
+          subtitle
+          featuredImage {
+            id
+          }
+          buttons {
+            buttonTitle
+            buttonUrl
+          }
+        }
+        highlights {
+          title
+          icon {
+            id
+          }
+        }
+        Testimonials {
+          description
+          title
+          buttonTitle
+          buttonUrl
+          featuredTestimonials {
+            content
+            name
+            image {
+              id
+            }
+          }
         }
       }
     }
