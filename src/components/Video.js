78745c79047a0fd8e112194eda6187da4e44ca8e
Amber Gowans <amber@thriveweb.com.au>
2018-08-30 11:50:21 +1000

    video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,22 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Image from './Image'
 
 import './Video.css'
 
-export default ({ video = '', title, videoOverlay = '', className }) => {
+class Video extends Component {
+	constructor(props) {
+		super(props)
 
-	if(!video) return null
+		this.videoRef = React.createRef()
 
-	if (videoOverlay) className += ' videoOverlay'
+		this.state = {}
+	}
 
-	return <div className={`video-section ${className}`}>
-		{title && 
-			<div className='overlay-content'>
-				<h2 className='title-gradient'>{title}</h2>
-				<p className='button buttonWhite'>Watch Video</p>
-			</div>
-		}
-		<video className='video'>
-			<source src={video} type="video/mp4"/>
-		</video>
-	</div>
+	handleVideo = () => {
+		const video = {video}
+		const url = video.replace(/^.+v=/,'').replace(/\&.*/,'')
+
+		this.videoRef.current.src = `https://www.youtube.com/embed/${url}?autoplay=1&start=0&modestbranding=1&controls=0&disablekb=1&rel=0`
+		this.setState({
+			videoPlaying: true
+		})
+	}
+
+	render() {
+		const { title, video, imageOverlay } = this.props
+		const { videoPlaying } = this.state
+
+		if(!video) return null
+
+		// if (videoBanner) className += ' videoBanner'
+
+		const url = video.replace(/^.+v=/,'').replace(/\&.*/,'')
+
+		
+
+		return <div className={`video-section`}>
+			{title && !videoPlaying &&
+				<div className='overlay-content'>
+					<Image background src={imageOverlay} />
+					<h2 
+						className='title-gradient'
+						onClick={() => this.handleVideo()}
+					>
+						{title}
+					</h2>
+				</div>
+			}
+			<iframe 
+				ref={this.videoRef}
+				src={``} 
+				frameBorder="0" 
+				allowFullScreen
+			>
+			</iframe>
+		</div>
+	}
 }
+
+export default Video
