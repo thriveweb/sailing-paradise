469d8b875fc96877e08a6a18761709183afa474a
Amber Gowans <amber@thriveweb.com.au>
2018-09-01 13:34:09 +1000

    test

--- src/components/FeaturedTestimonial.js
+++ src/components/FeaturedTestimonial.js
@@ -6,20 +6,22 @@ import Content from './Content'
 import './FeaturedTestimonial.css'
 
 export default ({ description, title, buttonTitle, buttonUrl, featuredTestimonial }) => {
+
+    if(!featuredTestimonial) return null
+        
     const { name, content, image } = featuredTestimonial
 
-        return <section className='featuredTestimonials'>
-            <div className='testimonialIntro'>
-                {title && <h2>{title}</h2>}
-                {description && <Content src={description} />}
-                {buttonTitle && buttonUrl && <Button title={buttonTitle} url={buttonUrl} />}
-            </div>
-            <div className='testimonial'>
-                {image && <Image src={image} alt='' />}   
-                {name && <p className='title'>{name}</p>}
-                {content && <Content src={content} />}
-            </div>
-        </section>    
-    }
+    return <section className='featuredTestimonials'>
+        <div className='testimonialIntro'>
+            {title && <h2>{title}</h2>}
+            {description && <Content src={description} />}
+            {buttonTitle && buttonUrl && <Button title={buttonTitle} url={buttonUrl} />}
+        </div>
+        <div className='testimonial'>
+            {image && <Image src={image} alt='' />}   
+            {name && <p className='title'>{name}</p>}
+            {content && <Content src={content} />}
+        </div>
+    </section>    
 }
 
