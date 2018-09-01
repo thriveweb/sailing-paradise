a35b2efccc1640c0b772d84495e62badffa0f957
Amber Gowans <amber@thriveweb.com.au>
2018-09-01 13:55:40 +1000

    test

--- src/templates/SingleBoatTour.js
+++ src/templates/SingleBoatTour.js
@@ -6,7 +6,7 @@ import Link from 'gatsby-link'
 
 import PageHeader from '../components/PageHeader'
 import IntroText from '../components/IntroText'
-import ContentBox from '../components/ContentBox'
+import BoxContent from '../components/BoxContent'
 import GallerySlider from '../components/GallerySlider'
 import Accordion from '../components/Accordion'
 import ColumnBanner from '../components/ColumnBanner'
@@ -37,7 +37,7 @@ export const SingleBoatTourTemplate = ({
     <div className='BoatTourIntro'>
       <div className='container'>
         <IntroText content={intro} />
-        <ContentBox {...contentBox} />
+        <BoxContent {...contentBox} />
       </div>  
     </div> 
     <GallerySlider gallery={gallery} />
