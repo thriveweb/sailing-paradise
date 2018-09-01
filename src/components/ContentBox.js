7f5e9c5967d61c37a1c97107b7dd0fe6a6c52c6d
Amber Gowans <amber@thriveweb.com.au>
2018-09-01 14:30:34 +1000

    latest

--- src/components/ContentBox.js
+++ src/components/ContentBox.js
@@ -4,6 +4,9 @@ import Button from './Button'
 import './ContentBox.css'
 
 export default ({ title, buttonTitle, buttonUrl }) => {
+
+	if(!title) return null
+
 	return <div className='contentBox'>
 		<div className='contentBox-container'>
 			{title && <h3>{title}</h3>}
