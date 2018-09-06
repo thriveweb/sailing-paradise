6388fb4422a91093dbdeb8d2133e855ddfb17429
Amber Gowans <amber@thriveweb.com.au>
2018-09-06 16:02:58 +1000

    booking iframe

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -33,13 +33,13 @@ export const HomePageTemplate = ({
 }) => {
 
   return <main className='Home'>
-      <Video 
+      {/*<Video 
         video={featuredVideo} 
         homeVideo title={title} 
         featuredSlider={featuredSlider} 
         featuredBanner={featuredBanner} 
         socialMedia={socialMedia}
-      />
+      />*/}
       <ServiceColumns 
         services={services} 
         serviceBanner={serviceBanner}
@@ -66,6 +66,8 @@ export const HomePageTemplate = ({
       <SubscribeForm />
     </main>
 }
+
+
 
 // Export Default HomePage for front-end
 const HomePage = ({ data: { page, posts, settings } }) => (
