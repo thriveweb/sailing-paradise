04c0a4eea4d4d58a104752f693dc51a127643626
Amber Gowans <amber@thriveweb.com.au>
2018-09-06 16:16:14 +1000

    charters relation

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -33,13 +33,13 @@ export const HomePageTemplate = ({
 }) => {
 
   return <main className='Home'>
-      <Video 
+      {/*<Video 
         video={featuredVideo} 
         homeVideo title={title} 
         featuredSlider={featuredSlider} 
         featuredBanner={featuredBanner} 
         socialMedia={socialMedia}
-      />
+      />*/}
       <ServiceColumns 
         services={services} 
         serviceBanner={serviceBanner}
