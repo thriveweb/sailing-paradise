487b42dd18dfeb8e7376000729ac97e8b85c1b12
Francesca <francesca@thriveweb.com.au>
2018-09-09 09:49:41 +1000

    error testing

--- src/components/SortArray.js
+++ src/components/SortArray.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 import PostSection from '../components/PostSection'
 
 class SortArray extends Component {
+  render() {
+    const { order, items } = this.props
 
-	render() {
-		const { order, items } = this.props
+    if (!order) return null
 
-		const orderedItems = order.map(order => 
-			items.find(item => order.tours === item.frontmatter.title)
-		)
+    const orderedItems = order.map(order =>
+      items.find(item => order.tours === item.frontmatter.title)
+    )
 
-		return <PostSection posts={orderedItems} boatTours />
-	}
+    return <PostSection posts={orderedItems} boatTours />
+  }
 }
 
 export default SortArray
