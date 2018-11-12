dbd0f9fb4eeb0efe27fc375d4f81c4f3c6c80e3a
Amber Gowans <amber.gowans@hotmail.com>
2018-11-12 14:30:57 -0800

    test preview

--- src/cms/preview-templates/AboutPagePreview.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { AboutPage } from '../../templates/AboutPage'
-
-const AboutPagePreview = ({ entry, widgetFor }) => <AboutPage {...entry.toJS().data} />
-
-export default AboutPagePreview
