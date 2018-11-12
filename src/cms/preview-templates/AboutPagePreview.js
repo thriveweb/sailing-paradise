b98fa9e61ed25dd383411daed52eff82e2be4f12
Amber Gowans <amber.gowans@hotmail.com>
2018-11-12 14:47:29 -0800

    test preview

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,17 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/AboutPage'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    {...entry.toJS().data}
-  />
-)
-
-AboutPagePreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
-}
+const AboutPagePreview = ({ entry, widgetFor }) => <AboutPageTemplate {...entry.toJS().data} />
 
 export default AboutPagePreview
