c1780e88b7efe69b6657e762f70a7e9d618206bc
Amber Gowans <amber.gowans@hotmail.com>
2018-11-12 14:58:50 -0800

    preview false on global

--- src/cms/preview-templates/AboutPagePreview.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { AboutPageTemplate } from '../../templates/AboutPage'
-
-const AboutPagePreview = ({ entry, widgetFor }) => <AboutPageTemplate {...entry.toJS().data} />
-
-export default AboutPagePreview
